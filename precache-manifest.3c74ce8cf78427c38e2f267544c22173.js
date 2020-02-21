2f52e9cb9ac34e6172f597e0354a5f68ba5e1adb
yunglleung1 <yungl.leung1@gmail.com>
2020-02-21 09:48:28 -0500

    Updates

---
+++ precache-manifest.3c74ce8cf78427c38e2f267544c22173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa55825af3d3c3a12d506529e73e4a8",
+    "url": "/job-wishlist/index.html"
+  },
+  {
+    "revision": "dd99b01d6913d87bea6e",
+    "url": "/job-wishlist/static/css/main.21235f02.chunk.css"
+  },
+  {
+    "revision": "721228741fddd9ac973b",
+    "url": "/job-wishlist/static/js/2.53775395.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/job-wishlist/static/js/2.53775395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd99b01d6913d87bea6e",
+    "url": "/job-wishlist/static/js/main.21d4bfe7.chunk.js"
+  },
+  {
+    "revision": "e9af1c7184a395e6a2df",
+    "url": "/job-wishlist/static/js/runtime-main.c0b34353.js"
+  }
+]);
